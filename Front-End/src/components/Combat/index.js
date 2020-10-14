db381b61098c14d1f16e25347e7fbe840d1705ae
W. Boosko <boosko80@gmail.com>
2020-10-14 11:07:50 +0200

    creation composant Combat

---
+++ Front-End/src/components/Combat/index.js
@@ -0,0 +1,14 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+
+// == Composant
+const Combat = () => (
+  <div>
+    Composant : Combat
+  </div>
+);
+
+// == Export
+export default Combat;
