bb53936dab781ba26d21abe5f302e3f0a865b70e
W. Boosko <boosko80@gmail.com>
2020-10-14 15:42:27 +0200

    conflit vraiment regle

--- Front-End/src/components/App/index.js
+++ Front-End/src/components/App/index.js
@@ -6,11 +6,8 @@ import Header from '../Header';
 const App = () => (
   <div className="app">
     <Header />
-<<<<<<< HEAD
     Autre chose :p
-=======
     Générons un conflit :)
->>>>>>> mentions-legales
   </div>
 );
 
