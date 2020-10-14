1ebfd994de4bcb422090ecd9f8714a43cd71ac67
W. Boosko <boosko80@gmail.com>
2020-10-14 15:22:59 +0200

    Text mentions legales mis a jour

--- Front-End/src/components/LegalNotices/index.js
+++ Front-End/src/components/LegalNotices/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './style.scss';
 
 const LegalNotices = () => (
-  <div className="legalNotices">LegalNotices</div>
+  <div className="legalNotices">LegalNotices texte a rajouter pour tester</div>
 );
 
 export default LegalNotices;
