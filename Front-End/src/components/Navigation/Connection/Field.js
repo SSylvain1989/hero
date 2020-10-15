4d3c2c360afbafa22061ddb57b910c48f7d3c8bb
W. Boosko <boosko80@gmail.com>
2020-10-15 09:43:57 +0200

    composant Connection static + composant Field

---
+++ Front-End/src/components/Navigation/Connection/Field.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+
+// == Composant
+const Field = () => (
+  <div className="field">
+    <p className="field-label">Titre de l'input</p>
+    <input className="field-input" type="text" placeholder="Placeholder" />
+  </div>
+);
+
+// == Export
+export default Field;
--- Front-End/src/components/Navigation/Connection/index.js
+++ Front-End/src/components/Navigation/Connection/index.js
@@ -2,12 +2,22 @@
 import React from 'react';
 
 // == Import
+import Field from './Field';
+
+// == Import scss
+import './connection.scss';
 
 // == Composant
 const Connection = () => (
-  <div>
-    Composant : Start
-  </div>
+  <>
+    <form className="connection">
+      <h1 className="connection-title">Je suis déjà joueur</h1>
+      <Field />
+      <Field />
+      <input className="connection-submit" type="submit" value="C'est parti" />
+      <button className="connection-redirect" type="button">L'inscription c'est par ici</button>
+    </form>
+  </>
 );
 
 // == Export
