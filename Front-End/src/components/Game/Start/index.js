3af9b7a40e47b2f47efbc9b92f5be231326e4ae3
W. Boosko <boosko80@gmail.com>
2020-10-15 10:53:00 +0200

    composant Start static

--- Front-End/src/components/Game/Start/index.js
+++ Front-End/src/components/Game/Start/index.js
@@ -3,10 +3,27 @@ import React from 'react';
 
 // == Import
 
+// == Import scss
+import './start.scss';
+
 // == Composant
 const Start = () => (
-  <div>
-    Composant : Start
+  <div className="start">
+    <div className="start__scene">
+      <div className="start__scene--left">
+        <img
+          src=""
+          alt="Donneur de quète"
+        />
+      </div>
+      <div className="start__scene--right">
+        <p>Texte de description de l'histoire à suivre</p>
+        <div className="start__scene--rightbuttons">
+          <button type="button">Passer</button>
+          <button type="button">Démarrer</button>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
