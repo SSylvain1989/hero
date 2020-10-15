13f389a0051459fdaccb3eed2ecb0638430fc4d7
Caro-L-dev <liardcar@gmail.com>
2020-10-15 09:57:13 +0200

    Footer

--- Front-End/src/components/App/index.js
+++ Front-End/src/components/App/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 
 import './app.scss';
 import Header from '../Navigation/Header';
+import GamesList from '../Navigation/GamesList';
+import Contact from '../Navigation/Contact';
+import LegalNotices from '../Navigation/LegalNotices';
+import GameDetail from '../Navigation/GameDetail';
+import Footer from '../Navigation/Footer';
 
 const App = () => (
   <div className="app">
     <Header />
+    <GamesList />
+    <Contact />
+    <LegalNotices />
+    <GameDetail />
+    <Footer />
   </div>
 );
 
