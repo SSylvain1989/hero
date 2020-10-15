f6682799669131812f744a05be50b801bcf82218
Caro-L-dev <liardcar@gmail.com>
2020-10-15 14:55:52 +0200

    GamesList en cours

--- Front-End/src/components/App/index.js
+++ Front-End/src/components/App/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
 
 import './app.scss';
-import Header from '../Navigation/Header';
+// import Header from '../Navigation/Header';
 import GamesList from '../Navigation/GamesList';
-import Contact from '../Navigation/Contact';
-import LegalNotices from '../Navigation/LegalNotices';
-import GameDetail from '../Navigation/GameDetail';
-import Footer from '../Navigation/Footer';
+// import Contact from '../Navigation/Contact';
+// import LegalNotices from '../Navigation/LegalNotices';
+// import GameDetail from '../Navigation/GameDetail';
+//import Footer from '../Navigation/Footer';
 
 const App = () => (
   <div className="app">
-    <Header />
     <GamesList />
-    <Contact />
-    <LegalNotices />
-    <GameDetail />
-    <Footer />
   </div>
 );
 
--- Front-End/src/components/Navigation/GamesList/index.js
+++ Front-End/src/components/Navigation/GamesList/index.js
@@ -3,11 +3,11 @@ import React from 'react';
 import './style.scss';
 
 const GamesList = () => (
-  <div className="gamesList">GamesList
+  <div className="gamesList">
     <h1 className="gamesList__Title">Liste des jeux</h1>
     <div className="gamesList__Card">
-      <img className="gamesList__Card__img">Image</img>
-      <div className="gamesList__Card__infos">
+      <img src="#" alt="imgGames" className="gamesList__Card__Img"/> 
+      <div className="gamesList__Card__Infos">
         <h1 className="gamesList__Card__Title">Titre</h1>
         <p className="gamesList__Card__Difficulty">Difficulté</p>
         <p className="gamesList__Card__Theme">Thème</p>
