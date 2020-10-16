006c2241a6ce81813fb35fb3676912778b55b5be
SSylvain1989 <sylvain.sigonnez@gmail.com>
2020-10-16 17:43:42 +0200

    test requete axios

--- Back-End/app/controllers/connexioncontroller.js
+++ Back-End/app/controllers/connexioncontroller.js
@@ -7,18 +7,21 @@ const connexionController = {
         const messageTab = [];
         // request.body c'est ici qu'on récupère les infos du formulaire
         // on check si username rentré par l'user est égale à un User déjà dans la BDD
-        const checkUser = await user.findByUserName(request.body.userName); 
+        const checkUser = await user.findByUserName(request.body.userName);
 
         if (checkUser === undefined) {
             const messageUserName = 'UserName non enregistré en base de donnée';
-            messageTab.push({messageUserName: messageUserName});
+            messageTab.push({ messageUserName: messageUserName });
         };
+        console.log('je vais comparer les mdp', request.body);
+        console.log('checkUser', checkUser);
+
         // ici on compare le mdp reçu en front & celui déjà en BDD ( si true : c'est good)
         const compare = await bcrypt.compare(request.body.password, checkUser.password);
-        
+
         if (compare === false) {
             const messagePassword = `Le mot de passe n'est pas valide`;
-            messageTab.push({messagePassword: messagePassword});
+            messageTab.push({ messagePassword: messagePassword });
         };
         if (messageTab.length > 0) { // on check si notre tableau de message est supérieur à 0
             console.log(messageTab);
@@ -42,19 +45,19 @@ const connexionController = {
     },
     signup: async (request, response) => { //
         const messageTab = [];
-        console.log('req.body: ',request.body); // c'est ici qu'on récupère les info du formulaire
-        const checkUser = await user.findByUserName(request.body.userName); 
+        console.log('req.body: ', request.body); // c'est ici qu'on récupère les info du formulaire
+        const checkUser = await user.findByUserName(request.body.userName);
         if (checkUser !== undefined) { // - 1: On verifie si L'utilisateur existe en bdd
             const messageUserName = 'UserName deja enregistré en base de donnée';
-            messageTab.push({messageUserName: messageUserName});
+            messageTab.push({ messageUserName: messageUserName });
         };
         if (!emailValidator.validate(request.body.email)) { // - 2: On verifie si le format d'email est valide
             const messageEmail = `Cet email n\'est pas valide.`;
-            messageTab.push({messageEmail: messageEmail});
+            messageTab.push({ messageEmail: messageEmail });
         };
         if (request.body.password !== request.body.passwordConfirm) { // - 3: On verifie si le mdp et sa confirmation correspondent
             const messagePassword = "La confirmation du mot de passe ne correspond pas.";
-            messageTab.push({messagePassword: messagePassword});
+            messageTab.push({ messagePassword: messagePassword });
         };
         if (messageTab.length > 0) { // on check si notre tableau de message est supérieur à 0
             console.log(messageTab)
@@ -62,7 +65,7 @@ const connexionController = {
         };
         const salt = await bcrypt.genSalt(10); // 4 - On crypt le password
         const encryptedPassword = await bcrypt.hash(request.body.password, salt);
-          
+
         const newUser = { // 5 - on stock dans notre const newUser les informations reçus du front 
             userName: request.body.userName,
             email: request.body.email,
@@ -70,7 +73,7 @@ const connexionController = {
         };
 
         const save = await user.createUser(newUser); // 6 - on passe les informations en paramêtre de la fonction createUser
-        
+
         response.json(save); // 7 - on renvoi le RETURNING de la requete SQL , soit ici ( cf models user ) id & userName
     },
 };
