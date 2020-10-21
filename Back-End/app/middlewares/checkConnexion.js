f47e3cf6fa8ef435f5c0c1c9333aebce3965be19
Alexis <trane-@hotmail.fr>
2020-10-21 10:17:38 +0200

    ajout gestion de status code

--- Back-End/app/middlewares/checkConnexion.js
+++ Back-End/app/middlewares/checkConnexion.js
@@ -3,7 +3,7 @@ const { request } = require("express");
 const checkConnexion = (request, response, next) => {
     if(request.session.user.connected_user === false){
         const messageCheckConnection = 'Vous devez etre connecter pour accéder à cette page';
-        return response.json({message: messageCheckConnection, session: request.session.user});
+        return response.status(404).json({message: messageCheckConnection, session: request.session.user});
     }
   
     if(request.session.user.connected_user === true) {
