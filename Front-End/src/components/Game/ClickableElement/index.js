0c75af4c587a6161179b43e39ec73527c616c28c
W. Boosko <boosko80@gmail.com>
2020-10-21 16:12:27 +0200

    clickableElement scene2

--- Front-End/src/components/Game/ClickableElement/index.js
+++ Front-End/src/components/Game/ClickableElement/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
+import { Link, useParams, Redirect } from 'react-router-dom';
 
 import './clickableElement.scss';
 
-const clickableElement = () => (
-  <div className="clickable-element">
-    <div className="clickable-element__scene">
-      <h1>Lieu de la scène</h1>
-      <img
-        src=""
-        alt="Fiche personnage"
-      />
-    </div>
-  </div>
-);
+const clickableElement = ({ story }) => {
+  const { histoire, scene } = useParams();
+
+  if (story.history !== undefined) {
+    const description = story.history.scene_list[1].details_scene.scene_description;
+    const nextScene = `/histoires/${histoire}/scene/${story.history.scene_list[scene].next_scene_id}`;
+    const nextScene2 = `/histoires/${histoire}/scene/${story.history.scene_list[scene].next_scene_id2}`;
+
+    return (
+      <div className="clickable-element">
+        <div className="clickable-element__scene">
+          <h1>{description}</h1>
+          <img
+            src=""
+            alt="Fiche personnage"
+          />
+          <Link to={nextScene}><button className="clickable-element__redirect" type="button">Porte</button></Link>
+          <Link to={nextScene2}><button className="clickable-element__redirect" type="button">Vieillard</button></Link>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <Redirect to={`/histoires/${histoire}`} />
+  );
+};
 
 export default clickableElement;
