03e93a959129862bc7023d3c5ad0e25dce680ff3
SSylvain1989 <sylvain.sigonnez@gmail.com>
2020-10-21 13:57:36 +0200

    bouton-declenche-storie

--- Front-End/src/components/Game/Start/index.js
+++ Front-End/src/components/Game/Start/index.js
@@ -1,5 +1,5 @@
 // == Import npm
-import React, { useEffect } from 'react';
+import React from 'react';
 
 // == Import
 import startMinautor from '../../../images/start-minautor.gif';
@@ -8,37 +8,30 @@ import parchemin from '../../../images/parchemin.png';
 import './start.scss';
 
 // == Composant
-const Start = ({ loadStory }) => {
-  useEffect(() => {
-    // qqch pour récupérer l'histoire
-    loadStory();
-  }, []);
-
-  return (
-    <div className="start">
-      <div className="start__scene">
-        <img
-          src={startMinautor}
-          alt="mooving-minautor"
-        />
-        <div className="start__scene--left" />
-        <div className="start__scene--right">
-          <div className="start__scene--right-text">
-            <p>Texte de description de l'histoire à suivre</p>
-            <img
-              src={parchemin}
-              alt="parchemin"
-            />
-          </div>
-          <div className="start__scene--right-buttons">
-            <button type="button">Passer</button>
-            <button type="button">Démarrer</button>
-          </div>
+const Start = ({ loadStory }) => (
+  <div className="start">
+    <div className="start__scene">
+      <img
+        src={startMinautor}
+        alt="mooving-minautor"
+      />
+      <div className="start__scene--left" />
+      <div className="start__scene--right">
+        <div className="start__scene--right-text">
+          <p>Texte de description de l'histoire à suivre</p>
+          <img
+            src={parchemin}
+            alt="parchemin"
+          />
+        </div>
+        <div className="start__scene--right-buttons">
+          <button type="button">Passer</button>
+          <button type="button">Démarrer</button>
         </div>
       </div>
-    </div>)
-
-};
+    </div>
+  </div>
+);
 
 // == Export
 export default Start;
