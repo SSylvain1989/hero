15547e1b9f738803363ca5e859407af5ebb73a7a
NicolasVid <65497850+NicolasVid@users.noreply.github.com>
2020-10-26 17:02:09 +0100

    WithCredentials

--- Front-End/src/middleware/gameMiddleware.js
+++ Front-End/src/middleware/gameMiddleware.js
@@ -10,14 +10,16 @@ import {
 const gameMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
     case LOAD_ONE_STORY:
-      axios.get('http://34.207.247.234:3000/api/stories/1/play')
+      axios.get('http://34.207.247.234:3000/api/stories/1/play',
+        { withCredentials: true })
         .then((response) => {
           store.dispatch(addStory(response.data));
         });
       next(action);
       break;
     case LOAD_CHARACTER_LIST:
-      axios.get('http://34.207.247.234:3000/api/characters')
+      axios.get('http://34.207.247.234:3000/api/characters',
+        { withCredentials: true })
         .then((response) => {
           console.log(response.data);
           store.dispatch(addCharacterList(response.data));
