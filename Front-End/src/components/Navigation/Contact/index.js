c68832297c2e9e42dc853f6d9dfccf69422e077b
Caro-L-dev <liardcar@gmail.com>
2020-10-26 12:09:10 +0100

    Contact-component

--- Front-End/src/components/Navigation/Contact/index.js
+++ Front-End/src/components/Navigation/Contact/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable no-alert */
 /* eslint-disable react/button-has-type */
@@ -9,7 +11,14 @@ import './contact.scss';
 
 // https://medium.com/@barseetbrown/send-e-mail-directly-from-your-react-website-no-back-end-required-ed0d3106c3d4
 
-const Contact = () => {
+const Contact = (
+  {
+    email,
+    message,
+    changeField,
+    handleContact,
+  },
+) => {
   // const onSubmit = (e) => {
   //   e.preventDefault();
   //   emailjs.sendForm('gmail', apiKeys.TEMPLATE_ID, e.target, apiKeys.USER_ID)
@@ -23,6 +32,7 @@ const Contact = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();// Prevents default refresh by the browser
+    handleContact();
     const form = {
       name: 'toto', subject: 'test toto', message: 'toto text',
     };
@@ -41,11 +51,24 @@ const Contact = () => {
   return (
     <form className="contact" onSubmit={onSubmit}>
       <h1>Nous contacter</h1>
-      <input name="email" type="text" placeholder="Votre email" className="contact__input" />
-      <textarea name="message" type="text" placeholder="Votre message ..." className="contact__input-message" />
+      <input name="email" type="text" value={email} onChange={changeField} placeholder="Votre email" className="contact__input" />
+      <textarea name="message" value={message} type="text" placeholder="Votre message ..." className="contact__input-message" />
       <button className="contact__input — button">Envoyer</button>
     </form>
   );
 };
+
+Contact.propTypes = ({
+  email: PropTypes.string,
+  message: PropTypes.string,
+  changeField: PropTypes.func.isRequired,
+  handleContact: PropTypes.func.isRequired,
+});
+
+Contact.defaultProps = ({
+  email: '',
+  // eslint-disable-next-line react/default-props-match-prop-types
+  message: '',
+});
 
 export default Contact;
