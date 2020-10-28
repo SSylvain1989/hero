c8df61d27fcbc532505312166faf5baebd4823c6
Caro-L-dev <liardcar@gmail.com>
2020-10-28 14:54:47 +0100

    Css-legal-notices-suite

--- Front-End/src/components/Navigation/LegalNotices/index.js
+++ Front-End/src/components/Navigation/LegalNotices/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from 'react';
 import backgroundhero from 'src/images/backgroundhero.jpg';
 
@@ -5,9 +6,13 @@ import './legalnotices.scss';
 
 const LegalNotices = () => (
   <div className="legal-notices">
-    <h1 className="legal-notices__title">Mentions légales</h1>
+    <img className="legal-notices__bg--site" src={backgroundhero} alt="backgroundHero" />
     <div className="legal-notices__encart">
-      <p className="legal-notices__text">Bla - Bla - Bla</p>
+      <h1 className="legal-notices__title">Mentions légales</h1>
+      <p className="legal-notices__text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula, turpis nec congue pharetra, eros metus hendrerit arcu, id pretium quam arcu vel sapien. Integer iaculis purus et ante vulputate aliquet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a augue sed dui posuere elementum. Phasellus convallis tempor sem, vitae fermentum erat semper eu. Ut et aliquet risus. Maecenas lectus lacus, blandit convallis molestie eu, ultrices at odio. Fusce tempor scelerisque ligula sed facilisis. Integer non vehicula eros. Proin efficitur scelerisque odio, vel efficitur lectus mollis at. Ut blandit diam at ante sollicitudin, id gravida turpis tempor. Curabitur venenatis posuere augue, sit amet gravida sapien vestibulum a.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula, turpis nec congue pharetra, eros metus hendrerit arcu, id pretium quam arcu vel sapien. Integer iaculis purus et ante vulputate aliquet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a augue sed dui posuere elementum. Phasellus convallis tempor sem, vitae fermentum erat semper eu. Ut et aliquet risus. Maecenas lectus lacus, blandit convallis molestie eu, ultrices at odio. Fusce tempor scelerisque ligula sed facilisis. Integer non vehicula eros. Proin efficitur scelerisque odio, vel efficitur lectus mollis at. Ut blandit diam at ante sollicitudin, id gravida turpis tempor. Curabitur venenatis posuere augue, sit amet gravida sapien vestibulum a.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula, turpis nec congue pharetra, eros metus hendrerit arcu, id pretium quam arcu vel sapien. Integer iaculis purus et ante vulputate aliquet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a augue sed dui posuere elementum. Phasellus convallis tempor sem, vitae fermentum erat semper eu. Ut et aliquet risus. Maecenas lectus lacus, blandit convallis molestie eu, ultrices at odio. Fusce tempor scelerisque ligula sed facilisis. Integer non vehicula eros. Proin efficitur scelerisque odio, vel efficitur lectus mollis at. Ut blandit diam at ante sollicitudin, id gravida turpis tempor. Curabitur venenatis posuere augue, sit amet gravida sapien vestibulum a.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula, turpis nec congue pharetra, eros metus hendrerit arcu, id pretium quam arcu vel sapien. Integer iaculis purus et ante vulputate aliquet. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
     </div>
   </div>
 );
