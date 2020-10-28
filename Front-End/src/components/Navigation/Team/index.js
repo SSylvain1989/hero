f3069f575540ab3ae2148736214cbf67d66d7ea8
SSylvain1989 <sylvain.sigonnez@gmail.com>
2020-10-28 17:11:35 +0100

    css-team-2

--- Front-End/src/components/Navigation/Team/index.js
+++ Front-End/src/components/Navigation/Team/index.js
@@ -3,23 +3,50 @@ import React from 'react';
 
 // == Import
 import Member from './Member';
+import team from '../../../data/team';
 
 // == Import scss
 import './team.scss';
 
 // == Composant
-const Team = () => (
-  <div>
-    <h1 className="heading"> La Team Hero</h1>
-    <div className="team">
-      <Member />
-      <Member />
-      <Member />
-      <Member />
-      <Member />
+const Team = () => {
+  console.log(team);
+
+  const teamList = team.map((oneTeamMember) => {
+    console.log(oneTeamMember.description);
+
+    return (
+      <div className="connasse">
+        <div>{oneTeamMember.description}</div>
+        <div>{oneTeamMember.fullName}</div>
+        {/* <Member
+          key={oneTeamMember.fullName}
+          name={oneTeamMember.fullName}
+          specialite={oneTeamMember.specilite}
+          role={oneTeamMember.role}
+          description={oneTeamMember.description}
+        /> */}
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      {teamList}
+      {/* <h1 className="heading"> La Team Hero</h1>
+      <div className="team">
+        <Member />
+        <Member />
+        <Member />
+        <Member />
+      </div> */}
     </div>
-  </div>
-);
+  );
+};
 
 // == Export
 export default Team;
+
+// {team.map((oneTeamMember) => (
+//   <h1>{oneTeamMember.name}</h1>
+// ))}
