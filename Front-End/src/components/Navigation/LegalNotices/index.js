205f2a05d5c09e8969b24eb7b46845fe1312d3a0
Caro-L-dev <liardcar@gmail.com>
2020-10-28 13:38:08 +0100

    Css-legal-notices-wip

--- Front-End/src/components/Navigation/LegalNotices/index.js
+++ Front-End/src/components/Navigation/LegalNotices/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import backgroundhero from 'src/images/backgroundhero.jpg';
 
-<link href="https://uvc.one/upload/iblock/ec9/ec9cac883ba503f2f7e49b3bef3f28b6.css" rel="stylesheet"></link>
-
 import './legalnotices.scss';
 
 const LegalNotices = () => (
   <div className="legal-notices">
-      <img className="Bg_heros" src={backgroundhero} alt=''></img>
-    <h1>Mentions légales</h1>
-    <div className="legal-notices_encart">
-      <p>Bla - Bla - Bla</p>
+    <h1 className="legal-notices__title">Mentions légales</h1>
+    <div className="legal-notices__encart">
+      <p className="legal-notices__text">Bla - Bla - Bla</p>
     </div>
   </div>
 );
