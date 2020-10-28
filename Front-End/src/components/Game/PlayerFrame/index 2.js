8d4b6388af728ff1f77fa56922ad2ffd6e2e1a14
NicolasVid <65497850+NicolasVid@users.noreply.github.com>
2020-10-28 14:27:31 +0100

    Test

---
+++ Front-End/src/components/Game/PlayerFrame/index 2.js
@@ -0,0 +1,34 @@
+// == Import npm
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+// == Import
+import testImage from 'src/images/start-minautor.gif';
+import PlayerMenu from './PlayerMenu';
+
+// == Composant
+const PlayerFrame = ({ player }) => {
+  const [playerMenu, setPlayerMenu] = useState(false);
+
+  const togglePlayerMenu = () => {
+    setPlayerMenu(!playerMenu);
+  };
+
+  return (
+    <div className="player-frame">
+      <div className="player-frame__click" onClick={togglePlayerMenu} />
+      <PlayerMenu isToggled={playerMenu} {...player} />
+    </div>
+  );
+};
+
+// == PropTypes
+PlayerFrame.propTypes = {
+  player: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+// == Export
+export default PlayerFrame;
