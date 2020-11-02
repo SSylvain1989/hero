5ae28ed708ddd234ea5e8fb0786229183e831249
SSylvain1989 <sylvain.sigonnez@gmail.com>
2020-11-02 12:21:42 +0100

    css-header

--- Front-End/src/components/Navigation/Header/index.js
+++ Front-End/src/components/Navigation/Header/index.js
@@ -11,81 +11,82 @@ const Header = ({
   logoutHandler,
   userName,
 }) => (
-  <div className="header">
-    <ul>
-      <div className="header__leftside">
-        <NavLink
-          to="/"
-          exact
-        >
-          <img
-            src={logo}
-            alt="logo"
-          />
-        </NavLink>
-        <li>
+    <div className="header">
+      <ul>
+        <div className="header__leftside">
           <NavLink
-            to="/liste-des-jeux"
+            to="/"
             exact
           >
-            Liste des jeux
+            <img
+              src={logo}
+              alt="logo"
+            />
           </NavLink>
-        </li>
-        {isLoged
-          ? (
-            <li>
-              <NavLink
-                to="/score"
-                exact
-              >
-                Scores
-              </NavLink>
-            </li>
-          ) : null}
-      </div>
-      <div className="header__rightside">
-        {isLoged
-          ? (
-            <>
-              <li>
-                {userName}
-              </li>
+          <li>
+            <NavLink
+              to="/liste-des-jeux"
+              exact
+            >
+              Liste des jeux
+          </NavLink>
+          </li>
+          {isLoged
+            ? (
               <li>
                 <NavLink
-                  to="/profil"
+                  to="/score"
                   exact
                 >
-                  <img
-                    src="https://i.ibb.co/ZXpv3t3/minotaur.gif"
-                    alt="avatar"
-                  />
-                </NavLink>
+                  Scores
+              </NavLink>
               </li>
-              <li onClick={logoutHandler}><NavLink to="/">Déconnexion</NavLink></li>
-            </>
-          )
-          : (
-            <>
-              <li>
-                <NavLink
-                  to="/Connection"
-                  exact
-                >Connexion
+            ) : null}
+        </div>
+        <div className="header__rightside">
+          {isLoged
+            ? (
+              <>
+                <li>
+
+                </li>
+                <li>
+                  <NavLink
+                    to="/profil"
+                    exact
+                  >
+                    <p>{userName}</p>
+                    <img
+                      src="https://i.ibb.co/ZXpv3t3/minotaur.gif"
+                      alt="avatar"
+                    />
+                  </NavLink>
+                </li>
+                <li onClick={logoutHandler}><NavLink to="/">Déconnexion</NavLink></li>
+              </>
+            )
+            : (
+              <>
+                <li>
+                  <NavLink
+                    to="/Connection"
+                    exact
+                  >Connexion
                 </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/inscription"
-                  exact
-                >Créer un compte
+                </li>
+                <li>
+                  <NavLink
+                    to="/inscription"
+                    exact
+                  >Créer un compte
                 </NavLink>
-              </li>
-            </>
-          )}
-      </div>
-    </ul>
-  </div>
-);
+                </li>
+              </>
+            )}
+        </div>
+      </ul>
+    </div>
+  );
 
 Header.propTypes = ({
   isLoged: PropTypes.bool.isRequired,
